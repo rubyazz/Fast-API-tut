e740fc9be4527f07dda5a8823740f82fee966b4c
rubyazz <ersultan.abduvalov@gmail.com>
2023-05-07 19:30:40 +0600

    initial commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+	name:str
+	price:float
+	is_offer: Union[bool, None] = None
+		
+
+@app.get("/")
+def read_root():
+	return {"Hello":"world!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id : int, q: Union[str, None] = None):
+	return {"item_id":item_id, "q":q}
+
+@app.put("/items/{item_id}")
+def read_item(item_id : int, item: Item):
+	return {"item_name":item_name, "item_id":item_id}
+
+@app.get("/routes")
+def get_routes():
+	return {"Cool ":"ROUTES"}
+
+
